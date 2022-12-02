29d24836a61764b42cee0798f22d87df9ba58424
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-12-02 11:57:15 -0500

    pod Almofire--http GET request to wikipedia

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -2,169 +2,83 @@
 //  ViewController.swift
 //  WhatFlower
 //
-//  Created by Angela Yu on 01/07/2017.
-//  Copyright © 2017 Angela Yu. All rights reserved.
+//  Created by user215496 on 11/29/22.
 //
 
 import UIKit
 import CoreML
 import Vision
-import SwiftyJSON
 import Alamofire
-import SDWebImage
-import ColorThiefSwift
+import SwiftyJSON
 
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    @IBOutlet weak var scrollView: UIScrollView!
-    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
-    var pickedImage : UIImage?
-    
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
     @IBOutlet weak var imageView: UIImageView!
-    
-    @IBOutlet weak var infoLabel: UILabel!
+    let wikipediaURL = "https://en.wikipedia.org/w/api.php"
     let imagePicker = UIImagePickerController()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Do any additional setup after loading the view.
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
         imagePicker.allowsEditing = true
+       // imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
         
     }
-    
-    
-    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        
-        if let userPickedImage = info[.originalImage] as? UIImage {
-            
-            guard let ciImage = CIImage(image: userPickedImage) else {
-                fatalError("Could not convert image to CIImage.")
+        if let userPickedImage = info[.editedImage] as? UIImage{
+            //set imageView background
+            imageView.image = userPickedImage
+            //convert into CIImage
+            guard let convertedciimage = CIImage(image: userPickedImage) else{
+                fatalError("Can't convert UIImage into CIImage")
             }
+            detect(image: convertedciimage)
             
-            pickedImage = userPickedImage
-            
-            
-            detect(flowerImage: ciImage)
         }
-        
-        imagePicker.dismiss(animated: true, completion: nil)
-        
-        
+        imagePicker.dismiss(animated: true)
         
     }
-    
-    func detect(flowerImage: CIImage) {
-        
-        
-        
-        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
-            fatalError("Can't load model")
+    func detect(image: CIImage ){
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else{
+            fatalError("cannot import model")
         }
-        
-        let request = VNCoreMLRequest(model: model) { (request, error) in
-            guard let result = request.results?.first as? VNClassificationObservation else {
-                fatalError("Could not complete classfication")
+        let request = VNCoreMLRequest(model: model){(request,error) in
+            guard let classification = request.results?.first as? VNClassificationObservation else{
+                fatalError("Could not classify")
             }
-      
-            self.navigationItem.title = result.identifier.capitalized
-            
-            self.requestInfo(flowerName: result.identifier)
-            
+            self.navigationItem.title = classification.identifier.capitalized
+            self.requestInfo(flowerName: classification.identifier)
         }
-        
-        let handler = VNImageRequestHandler(ciImage: flowerImage)
-        
-        do {
-            try handler.perform([request])
-        }
-        catch {
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([request])}
+        catch{
             print(error)
         }
-        
-        
     }
-    
-    func requestInfo(flowerName: String) {
-        let parameters : [String:String] = ["format" : "json", "action" : "query", "prop" : "extracts|pageimages", "exintro" : "", "explaintext" : "", "titles" : flowerName, "redirects" : "1", "pithumbsize" : "500", "indexpageids" : ""]
-        
-        
-        // https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|pageimages&exintro=&explaintext=&titles=barberton%20daisy&redirects=1&pithumbsize=500&indexpageids
-        
-        Alamofire.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
-            if response.result.isSuccess {
-                //                print(response.request)
-                //
-                //                print("Success! Got the flower data")
-                let flowerJSON : JSON = JSON(response.result.value!)
-                
-                let pageid = flowerJSON["query"]["pageids"][0].stringValue
-                
-                let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
-                let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
-                
-                //                print("pageid \(pageid)")
-                //                print("flower Descript \(flowerDescription)")
-                //                print(flowerJSON)
-                //
-                self.infoLabel.text = flowerDescription
-                
-                
-                
-                
-                self.imageView.sd_setImage(with: URL(string: flowerImageURL), completed: { (image, error,  cache, url) in
-                    
-                    if let currentImage = self.imageView.image {
-                        
-                        guard let dominantColor = ColorThief.getColor(from: currentImage) else {
-                            fatalError("Can't get dominant color")
-                        }
-                        
-                        
-                        DispatchQueue.main.async {
-                            self.navigationController?.navigationBar.isTranslucent = true
-                            self.navigationController?.navigationBar.barTintColor = dominantColor.makeUIColor()
-                            
-                            
-                        }
-                    } else {
-                        self.imageView.image = self.pickedImage
-                        self.infoLabel.text = "Could not get information on flower from Wikipedia."
-                    }
-                    
-                })
-                
-            }
-            else {
-                print("Error \(String(describing: response.result.error))")
-                self.infoLabel.text = "Connection Issues"
-                
-                
-                
-            }
+    //Almofire http request
+    func requestInfo(flowerName: String){
+        let parameters:[String:String] = [
+            "format": "json",
+            "action":"query",
+            "prop":"extracts",
+            "exintro":"",
+            "explaintext":"",
+            "titles":"flowerName",
+            "indexpageids":"",
+            "redirects":"1"]
+        AF.request(wikipediaURL, method: .get,parameters:parameters).responseJSON { response in
+            debugPrint(response)
+            
         }
-    }
-    
-    
-    
-    @IBAction func cameraTapped(_ sender: Any) {
         
-        self.present(self.imagePicker, animated: true, completion: nil)
+    }
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true)
     }
     
 }
 
-
-
-
-
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-	return input.rawValue
-}
